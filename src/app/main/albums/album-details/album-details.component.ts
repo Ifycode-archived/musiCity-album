d28cd9779854b358297b0cc283f1efc550001588
ifycode <marychord@gmail.com>
2020-12-29 00:31:10 -0800

    Make album details pages display different tracks

--- src/app/main/albums/album-details/album-details.component.ts
+++ src/app/main/albums/album-details/album-details.component.ts
@@ -3,14 +3,25 @@ import { AlbumDetailsService } from '../specific/album-details.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  template: `<h2>Album Details Page: {{list?.id}}</h2>`
+  template: `
+    <h2>Album Details Page: {{list?.id}}</h2>
+    <div>
+      <div>{{list?.album?.name}}</div>
+      <div *ngFor="let track of list?.album?.tracks">
+        <img [src]="track.trackPhoto">
+      </div>
+    </div>
+  `
 })
 export class AlbumDetailsComponent implements OnInit {
   list: any;
+
   constructor(private _albumDetailsService: AlbumDetailsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this. _albumDetailsService.getAlbumList(+this.route.snapshot.params['id'])
+    /*this. _albumDetailsService.getAlbumList(+this.route.snapshot.params['id'])
+    .subscribe(response => this.list = response);*/
+    this. _albumDetailsService.getAlbum(+this.route.snapshot.params['id'])
     .subscribe(response => this.list = response);
   }
 
