51ad7c34ff59ca6058f1ef05458a89cfc51ccf26
ifycode <marychord@gmail.com>
2020-12-28 18:59:15 -0800

    Add activated route for page route when id number changes

--- src/app/main/albums/album-details/album-details.component.ts
+++ src/app/main/albums/album-details/album-details.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AlbumDetailsService } from '../specific/album-details.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   template: `<h2>Album Details Page: {{list?.id}}</h2>`
 })
 export class AlbumDetailsComponent implements OnInit {
   list: any;
-  constructor(private _albumDetailsService: AlbumDetailsService) { }
+  constructor(private _albumDetailsService: AlbumDetailsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this. _albumDetailsService.getAlbumList(1)
+    this. _albumDetailsService.getAlbumList(+this.route.snapshot.params['id'])
     .subscribe(response => this.list = response);
   }
 
